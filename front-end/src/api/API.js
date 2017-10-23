03c8c9f451a7f4cec8ea728a01c91a3297c53a7f
sonijigar <jigarsoni1111@gmail.com>
2017-10-22 18:45:14 -0700

    mongo with session

---
+++ front-end/src/api/API.js
@@ -0,0 +1,41 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
+
+const headers = {
+    'Accept': 'application/json'
+};
+
+export const doLogin = (payload) =>
+    fetch(`${api}/login`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        credentials:'include',
+        body: JSON.stringify(payload)
+    }).then(res => {
+        console.log("respon", window)
+        window.sessionStorage.setItem("key", payload.username);
+        return res.status;
+    })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+
+export const logout = () =>
+    fetch(`${api}/logout`, {
+        method: 'POST',
+        headers: {
+            ...headers
+        },
+        credentials:'include'
+    }).then(res => {
+        window.sessionStorage.removeItem('key');
+        return res.status;
+        //  res.data;
+    })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
